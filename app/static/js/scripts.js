4910bc447e27af1aadea9b24d536b9fa50966dbc
vas <vasile.vio200@gmail.com>
2024-07-11 03:52:09 +0300

    Finished navbar and menu for now

--- app/static/js/scripts.js
+++ app/static/js/scripts.js
@@ -8,3 +8,22 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 });
+
+// Turns the hamburger menu into a X after pressing it and opens the menu
+// from the right, and then undoing everything if pressed 2nd time
+let changer = true;
+function menuDictating() {
+    var menu_active = document.getElementById("menu__body"); 
+    var menu_opener = document.getElementById("menu__opener");
+    var menu_closer = document.getElementById("menu__closer");
+    if (changer) {
+        menu_active.style.transform = 'translate(0)';
+        menu_opener.style.display = 'none';
+        menu_closer.style.display = 'flex';
+    } else {
+        menu_active.style.transform = 'translate(-5000px)';
+        menu_opener.style.display = 'flex';
+        menu_closer.style.display = 'none';
+    }
+    changer = !changer;
+}
