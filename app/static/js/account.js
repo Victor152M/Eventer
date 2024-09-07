46d67c597c0dc57b1435dbad0a4a07fc65333b23
Victor152M <stanciuvictorcristian@yahoo.com>
2024-09-07 11:51:17 +0300

    event removal confirmation dialog box

--- app/static/js/account.js
+++ app/static/js/account.js
@@ -3,7 +3,7 @@ function showLogoutConfirmation() {
     document.getElementById('logoutModal').style.display = 'block';
 }
 
-function closeModal() {
+function closeLogoutModal() {
     document.getElementById('logoutModal').style.display = 'none';
 }
 function logout() {
@@ -19,11 +19,49 @@ window.onclick = function(event) {
     }
 }
 
+// remove event modal
+let confirmResolve = null;
+
+function showRemoveEventConfirmation() {
+    return new Promise((resolve) => {
+        confirmResolve = resolve;
+        document.getElementById('removeEventModal').style.display = 'block';
+    });
+}
+
+function cancelRemoval() {
+    document.getElementById('removeEventModal').style.display = 'none';
+    if (confirmResolve) {
+        confirmResolve(false);
+        confirmResolve = null;
+    }
+}
+
+function confirmRemoval() {
+    document.getElementById('removeEventModal').style.display = 'none';
+    if (confirmResolve) {
+        confirmResolve(true);
+        confirmResolve = null;
+    }
+}
+
+window.onclick = function(event) {
+    var modal = document.getElementById('removeEventModal');
+    if (event.target == modal) {
+        cancelRemoval();
+    }
+}
+
 // event listener for the remove button
 document.addEventListener('DOMContentLoaded', () => {
     document.querySelectorAll('.remove-event').forEach(button => {
         button.addEventListener('click', async function() {
             const eventId = this.getAttribute('data-id');
+            const confirmation = await showRemoveEventConfirmation();
+            if (!confirmation) {
+                return; // If the user cancels, stop the function
+            }
+            
             const response = await fetch('/api/account/remove_event', {
                 method: 'POST',
                 headers: {
