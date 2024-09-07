9f3432a83dc2c29b64439c126e9d7f3ca2f6c06c
Victor152M <stanciuvictorcristian@yahoo.com>
2024-09-07 21:06:53 +0300

    can access events from account page

--- app/static/js/account.js
+++ app/static/js/account.js
@@ -19,6 +19,17 @@ window.onclick = function(event) {
     }
 }
 
+document.addEventListener('DOMContentLoaded', () => {
+    const events = document.querySelectorAll('.event-card');
+    
+    events.forEach(event => {
+        event.addEventListener('click', () => {
+            const articleUrl = event.getAttribute('data-article');
+            window.location.href = articleUrl;
+        });
+    });
+});
+
 // remove event modal
 let confirmResolve = null;
 
